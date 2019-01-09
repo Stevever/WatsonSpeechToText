ebaf83e60760c08b4d6f5b0ed8f2988de11e3499
lpatel16 <patel.2434@buckeyemail.osu.edu>
2019-01-08 19:27:15 -0500

    added file from class

---
+++ speechtotext.py
@@ -0,0 +1,7 @@
+from watson_developer_cloud import SpeechToTextV1
+import json
+
+speech_to_text = SpeechToTextV1(
+    iam_apikey='FMdj05bLeFKPX95Ve-wStDFWYFXXuqZDXzLHsT4paUHh',
+    url='https://stream.watsonplatform.net/speech-to-text/api'
+)
